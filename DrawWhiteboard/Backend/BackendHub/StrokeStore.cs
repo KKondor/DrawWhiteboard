ef324512f44510a23a4273bb10e2527ea8b58dfa
KKondor <kkondor66@gmail.com>
2026-08-31 10:39:24 +0200

    feat: added AddPoint funvtion to StrokeStore.

--- DrawWhiteboard/Backend/BackendHub/StrokeStore.cs
+++ DrawWhiteboard/Backend/BackendHub/StrokeStore.cs
@@ -1,9 +1,24 @@
 ﻿using DrawWhiteboard.Backend.Types;
 
-public class StrokeStore
+namespace DrawWhiteboard.Backend.BackendHub
 {
-    private readonly List<Stroke> _strokes = new();
+    public class StrokeStore
+    {
+        private readonly List<Stroke> _strokes = new();
 
-    public void AddPoint(Point point) {}
-    public List<Stroke> GetAllStrokes() => _strokes;
+        public void AddPoint(Point point)
+        {
+            var stroke = _strokes.FirstOrDefault(s => s.StrokeId == point.StrokeId);
+            if (stroke is null)
+            {
+                stroke = new Stroke(point.StrokeId, point.Color, point.Thickness);
+                _strokes.Add(stroke);
+            }
+
+            stroke.Points.Add(point);
+            stroke.Points.Sort((a, b) => a.PointId.CompareTo(b.PointId));
+        }
+
+        public List<Stroke> GetAllStrokes() => _strokes;
+    }
 }
