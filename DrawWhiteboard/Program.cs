4cc9e377d6974e8bb698351449d602465ab643d4
KKondor <kkondor66@gmail.com>
2026-08-31 10:44:38 +0200

    feat: wired in Cors, Singleton and Hub

--- DrawWhiteboard/Program.cs
+++ DrawWhiteboard/Program.cs
@@ -1,14 +1,28 @@
 
+using DrawWhiteboard.Backend.BackendHub;
+
 namespace DrawWhiteboard
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            var MyAllowedSpecificOrigins = "_myAllowSpecificOrigins";
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddSingleton<StrokeStore>();
+            builder.Services.AddCors(opt =>
+            {
+                opt.AddPolicy(name: MyAllowedSpecificOrigins,
+                                  policy =>
+                                  {
+                                      policy.WithOrigins(builder.Configuration.GetConnectionString("DefaultFrontEnd"))
+                                      .AllowAnyHeader()
+                                      .AllowAnyMethod()
+                                      .AllowCredentials();
+                                  });
+            });
             var app = builder.Build();
-           
+            app.MapHub<WhiteBoardHub>("/whiteboardhub");
             app.Run();
         }
     }
